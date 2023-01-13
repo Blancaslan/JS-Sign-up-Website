958f062b5848a9f05ead336762007d7f3e78c7d8
Blancaslan <68610191+Blancaslan@users.noreply.github.com>
2023-01-13 13:59:04 +0000

    Changing color

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -5,21 +5,23 @@ export default function App() {
   return (
     <div className="outerbox">
       <div className="taskbar">
-      <Textbox id="username" field="username:"></Textbox>
-      <Textbox id="password" field="password:"></Textbox>
-      <Textbox id="firstname" field="firstname:"></Textbox>
-      <Textbox id="lastname" field="lastname:"></Textbox>
-      <Textbox id="age" field="age:"></Textbox>
+        <div>
+          <Textbox id="username" field="username:"></Textbox>
+          <Textbox id="password" field="password:"></Textbox>
+          <Textbox id="firstname" field="firstname:"></Textbox>
+          <Textbox id="lastname" field="lastname:"></Textbox>
+          <Textbox id="age" field="age:"></Textbox>
 
-      <button onClick={() => {
-        let username = document.querySelector("#username").value
-        let password = document.querySelector("#password").value
-        let firstname = document.querySelector("#firstname").value
-        let lastname = document.querySelector("#lastname").value
-        let age = document.querySelector("#age").value
-        let data = [username, password, firstname, lastname, age]
-        createAccount(data)
-      }}>Submit</button>
+          <button onClick={() => {
+            let username = document.querySelector("#username").value
+            let password = document.querySelector("#password").value
+            let firstname = document.querySelector("#firstname").value
+            let lastname = document.querySelector("#lastname").value
+            let age = document.querySelector("#age").value
+            let data = [username, password, firstname, lastname, age]
+            createAccount(data)
+          }}>Submit</button>
+        </div>
       </div>
     </div>
   )
